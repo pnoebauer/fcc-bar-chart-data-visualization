19e77ff53155a1ec76179fa4dbe3d769a64cc582
Philip Noebauer <pnoebauer>
2021-08-10 10:44:10 +0800

    finalized project

--- bar-chart/script.js
+++ bar-chart/script.js
@@ -1,96 +1,111 @@
-// const testData = [1, 2, 3];
-// d3.select('body')
-// 	.selectAll('div')
-// 	.data(testData)
-// 	.enter()
-// 	.append('div')
-// 	.attr('class', 'bar')
-// 	.style('margin', '2px');
+const tooltip = d3
+	.select('.container')
+	.append('div')
+	.attr('id', 'tooltip')
+	.style('opacity', 0);
 
-const tooltip = d3.select('body').append('div').attr('id', 'tooltip').style('opacity', 0);
+const w = 900;
+const h = 500;
 
-const w = 500;
-const h = 200;
-
-const padding = 22;
+const padding = 50;
 
 const svg = d3
-	.select('body')
+	.select('.container')
 	.append('svg')
 	.attr('height', h)
 	.attr('width', w)
-	.style('background-color', 'red');
+	.style('background-color', '#fff');
 
-const dataSet = [
-	['1947-01-01', 100],
-	['1947-03-01', 200],
-	['1947-05-01', 300],
-	['1947-07-01', 400],
-];
-
-const xScale = d3
-	// .scaleLinear()
-	.scaleTime()
-	// .domain([d3.min(dataSet, d => new Date(d[0])), d3.max(dataSet, d => new Date(d[0]))])
-	.domain([
-		d3.min(dataSet, d => new Date(d[0])),
-		new Date(d3.max(dataSet, d => new Date(d[0])).getTime() + 1000 * 60 * 60 * 24 * 31),
-	])
-	.range([padding, w - padding]);
-
-// console.log(d3.max(dataSet, d => d[0]));
-// console.log(new Date(d3.max(dataSet, d => d[0])));
-// console.log(d3.max(dataSet, d => new Date(d[0])));
-
-const yScale = d3
-	.scaleLinear()
-	// .domain([d3.min(dataSet, d => d[1]), d3.max(dataSet, d => d[1])])
-	.domain([0, d3.max(dataSet, d => d[1])])
-	.range([h - padding, padding]);
-
-// console.log([d3.min(dataSet, d => d[1]), d3.max(dataSet, d => d[1])], 'domain');
-
+// y-axis label
 svg
-	.selectAll('rect')
-	.data(dataSet)
-	.enter()
-	.append('rect')
-	.attr('x', d => xScale(new Date(d[0])))
-	.attr('y', d => console.log(d[1], yScale(d[1])) || yScale(d[1]))
-	.attr('data-date', d => d[0])
-	.attr('data-gdp', d => d[1])
-	.attr('width', 15)
-	.attr('height', d => h - yScale(d[1]) - padding)
-	.attr('class', 'bar')
-	// .on('mouseover', (d, i) => console.log(d, i));
-	.on('mouseover', (d, i) => {
-		console.log(d, i);
-		tooltip.transition().duration(200).style('opacity', 0.9);
-		tooltip
-			.html(i[0] + '<br/>' + i[1])
-			.attr('data-date', i[0])
-			.style('left', d.pageX + 'px')
-			.style('top', h - padding + 'px');
-		// .style('top', d.pageY - 28 + 'px');
-	})
-	.on('mouseout', d => {
-		tooltip.transition().duration(500).style('opacity', 0);
-	});
+	.append('text')
+	.attr('x', -300)
+	.attr('y', 80)
+	.attr('transform', 'rotate(-90)')
+	.text('Gross Domestic Product');
 
-const xAxis = d3.axisBottom(xScale).ticks();
-
+// x-axis label (info)
 svg
-	.append('g')
-	.attr('transform', `translate(0, ${h - padding})`)
-	.attr('id', 'x-axis')
-	.call(xAxis);
-// .call(xAxis.ticks(d3.timeMonth));
+	.append('text')
+	.attr('x', 400)
+	.attr('y', 490)
+	.text('More Information: http://www.bea.gov/national/pdf/nipaguid.pdf');
 
-const yAxis = d3.axisLeft(yScale);
-yAxis.ticks(2);
+async function plotBarsFromData() {
+	const res = await fetch(
+		'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json'
+	);
+	const projectData = await res.json();
+	// console.log(projectData);
 
-svg
-	.append('g')
-	.attr('transform', `translate(${padding}, 0)`)
-	.attr('id', 'y-axis')
-	.call(yAxis);
+	const dataSet = projectData.data;
+	// const dataSet = projectData.data.slice(0, 8);
+
+	const xScale = d3
+		.scaleTime()
+		.domain(d3.extent(dataSet, d => new Date(d[0])))
+		.range([padding, w - padding]);
+
+	// console.log(d3.max(dataSet, d => new Date(d[0])));
+
+	const yScale = d3
+		.scaleLinear()
+		.domain([0, d3.max(dataSet, d => d[1])])
+		.range([h - padding, padding]);
+
+	// console.log((w - 2 * padding) / dataSet.length, width per bar);
+
+	const convertToQuarter = date => {
+		const quarter = Math.floor(new Date(date).getMonth() / 4 + 1);
+		return `Q${quarter} ${new Date(date).getFullYear()}`;
+	};
+
+	svg
+		.selectAll('rect')
+		.data(dataSet)
+		.enter()
+		.append('rect')
+		.attr('x', d => xScale(new Date(d[0])))
+		.attr('y', d => yScale(d[1]))
+		.attr('data-date', d => d[0])
+		.attr('data-gdp', d => d[1])
+		.attr('width', (1 * (w - 2 * padding)) / dataSet.length)
+		.attr('height', d => h - yScale(d[1]) - padding)
+		.attr('class', 'bar')
+		.attr('fill', 'rgb(51, 173, 255)')
+		.on('mouseover', function (d, i) {
+			// d3.select(this).style('fill', 'green');
+			// console.log(d, i);
+			tooltip.transition().duration(200).style('opacity', 0.9);
+			tooltip
+				.html(`${convertToQuarter(i[0])} <br/> $${i[1]} Billion`)
+				.attr('data-date', i[0])
+				// .style('left', d.pageX + 'px')
+				.style('left', d.offsetX + 'px')
+				.style('top', h - padding + 'px');
+		})
+		.on('mouseout', function (d, i) {
+			// d3.select(this).style('fill', 'black');
+			tooltip.transition().duration(500).style('opacity', 0);
+		});
+
+	const xAxis = d3.axisBottom(xScale).ticks();
+
+	svg
+		.append('g')
+		.attr('transform', `translate(0, ${h - padding})`)
+		.attr('id', 'x-axis')
+		.call(xAxis);
+	// .call(xAxis.ticks(d3.timeMonth));
+
+	const yAxis = d3.axisLeft(yScale);
+	yAxis.ticks(10);
+
+	svg
+		.append('g')
+		.attr('transform', `translate(${padding}, 0)`)
+		.attr('id', 'y-axis')
+		.call(yAxis);
+}
+
+plotBarsFromData();
